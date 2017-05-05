da040eb85397e82798d2dc3ccf1643bb8dd922b7
gfiorav <guido.fioravantti@gmail.com>
2017-05-05 15:14:42 +0200

    add dictionaries controller

--- app/controllers/dictionaries_controller.rb
+++ app/controllers/dictionaries_controller.rb
@@ -1,11 +1,33 @@
 class DictionariesController < ApplicationController
-  before_filter :load_dictionary, except: [:create]
+  before_filter :load_dictionary, only: [:show, :destroy]
+
+  rescue_from ActiveRecord::RecordInvalid do |exception|
+    render json: exception, status: :unprocessable_entity
+  end
+
+  def index
+    render json: current_reader.dictionaries
+  end
+
+  def show
+    render json: @dictionary
+  end
+
+  def create
+    Dictionary.create(locator: params[:locator], reader: current_reader)
+    head :no_content
+  end
+
+  def destroy
+    @dictionary.destroy
+    head :no_content
+  end
 
   private
 
   def load_dictionary
-    Dictionary.find(params[:id])
-  rescue ActiveRecord::RecordNotFound => e
+    @dictionary = Dictionary.find(params[:id])
+  rescue ActiveRecord::RecordNotFound => _exception
     head :not_found
   end
 end
