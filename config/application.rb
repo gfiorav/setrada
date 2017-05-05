aefa67705f08d10ec5e4210df8b2970a27e743df
gfiorav <guido.fioravantti@gmail.com>
2017-05-05 11:46:22 +0200

    fix end

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,7 @@ module Setrada
     config.api_only = true
 
     config.generators do |generator|
-        generator.orm(:active_record, primary_key_type: :uuid)
+      generator.orm(:active_record, primary_key_type: :uuid)
+    end
   end
 end
