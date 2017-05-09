2ddf7478bb5edf4f3f87dd67ff4f6e4fdda6a255
gfiorav <guido.fioravantti@gmail.com>
2017-05-09 16:02:08 +0200

    add helpers

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,4 +8,15 @@ class ActiveSupport::TestCase
   ActiveRecord::Migration.maintain_test_schema!
 
   # Add more helper methods to be used by all tests here...
+  def random_string(lenght: 12)
+    (0...lenght).map { ('a'..'z').to_a[rand(26)] }.join
+  end
+
+  def create_user(username: random_string, password: random_string)
+    User.create!(username: username, password: password)
+  end
+
+  def create_dictionary(user: create_user, locator: random_string)
+    Dictionary.create!(user: user, locator: locator)
+  end
 end
