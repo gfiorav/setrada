19b7c5654669f75bbf3644ec99f378e52dcc0d09
gfiorav <guido.fioravantti@gmail.com>
2017-05-09 15:31:30 +0200

    add tests fro user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,4 +1,24 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  test 'username presence' do
+    assert_raises(ActiveRecord::NotNullViolation) do
+      create_user(username: nil)
+    end
+  end
+
+  test 'password presence' do
+    assert_raises(ActiveRecord::RecordInvalid) do
+      create_user(password: nil)
+    end
+  end
+
+  test 'username uniqueness' do
+    random_username = random_string
+    create_user(username: random_username)
+
+    assert_raises(ActiveRecord::RecordInvalid) do
+      create_user(username: random_username)
+    end
+  end
 end
