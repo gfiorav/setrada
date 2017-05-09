02fcd99fa6d2f4028e3725cd32499bc5b699d6f1
gfiorav <guido.fioravantti@gmail.com>
2017-05-09 12:47:24 +0200

    move

---
+++ app/lib/json_web_token.rb
@@ -0,0 +1,17 @@
+module Lib
+  class JsonWebToken
+    class << self
+      def encode(payload, exp = 24.hours.from_now)
+        payload[:exp] = exp.to_i
+        JWT.encode(payload, Rails.application.secrets.secret_key_base)
+      end
+
+      def decode(token)
+        decoded = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+        HashWithIndifferentAccess.new(decoded)
+      rescue
+        nil
+      end
+    end
+  end
+end
