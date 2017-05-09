c95264139de1fecda76650c0af30a73070ee3f79
gfiorav <guido.fioravantti@gmail.com>
2017-05-09 11:59:02 +0200

    add helper class for jwt

---
+++ lib/custom/json_web_token.rb
@@ -0,0 +1,15 @@
+class JsonWebTokne
+  class << self
+    def encode(payload, exp = 24.hours.from_now)
+      payload[:exp] = exp.to_i
+      JWT.encode(payload, Rails.application.secrets.secret_key_base)
+    end
+
+    def decode(token)
+      decoded = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+      HashWithIndifferentAccess.new(decoded)
+    rescue
+      nil
+    end
+  end
+end
