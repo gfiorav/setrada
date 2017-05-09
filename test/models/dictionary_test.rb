6c7008ed568adfa6291358643afbc799ef4f24f2
gfiorav <guido.fioravantti@gmail.com>
2017-05-09 16:12:44 +0200

    add tests for dictionaries

--- test/models/dictionary_test.rb
+++ test/models/dictionary_test.rb
@@ -1,7 +1,38 @@
 require 'test_helper'
 
 class DictionaryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'locator presence' do
+    assert_raises(ActiveRecord::NotNullViolation) do
+      create_dictionary(locator: nil)
+    end
+  end
+
+  test 'user presence' do
+    assert_raises(ActiveRecord::RecordInvalid) do
+      create_dictionary(user: nil)
+    end
+  end
+
+  test 'locator uniqueness inside user scope' do
+    user = create_user
+
+    assert_raises(ActiveRecord::RecordInvalid) do
+      previous_dictionary = create_dictionary(user: user)
+      create_dictionary(user: user, locator: previous_dictionary.locator)
+    end
+
+    user.destroy
+  end
+
+  test 'locator duplication outside user scope' do
+    one_user = create_user
+    another_user = create_user
+    same_locator = random_string
+
+    create_dictionary(user: one_user, locator: same_locator)
+    create_dictionary(user: another_user, locator: same_locator)
+
+    one_user.destroy
+    another_user.destroy
+  end
 end
