058d941021cb1be8f2d88b10456c5bfbe7a5ab6e
gfiorav <guido.fioravantti@gmail.com>
2017-05-10 19:01:54 +0200

    add dummy seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create(username: 'guido', password: '123456')
+
+a_table_dict = Dictionary.create(user: user, locator: 'a table')
+a_map_dict = Dictionary.create(user: user, locator: 'a map')
+an_interface_dict = Dictionary.create(user: user, locator: 'an interface')
+
+a_table_map = {
+  'cartodb_id' => 'id',
+  'table_name_1234_ene_2013' => 'Results from elections'
+}
+
+Translation.create(dictionary: a_table_dict, map: a_table_map)
+
+a_map_map = {
+  'calculation' => 'results',
+  'max' => 'top',
+  'total_results' => 'Total results of the calculation'
+}
+
+Translation.create(dictionary: a_map_dict, map: a_map_map)
+
+spanish = {
+  'above' => 'arriba',
+  'all' => 'todo',
+  'results' => 'resultados'
+}
+
+Translation.create(dictionary: an_interface_dict, map: spanish)
