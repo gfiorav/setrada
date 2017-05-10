9ec4b5d3e54662fcd928aa9b0e107168a70109ae
gfiorav <guido.fioravantti@gmail.com>
2017-05-10 14:16:12 +0200

    add privacy

--- db/migrate/20170505100707_create_dictionaries.rb
+++ db/migrate/20170505100707_create_dictionaries.rb
@@ -2,6 +2,7 @@ class CreateDictionaries < ActiveRecord::Migration[5.1]
   def change
     create_table :dictionaries, id: :uuid do |t|
       t.string :locator, null: false
+      t.integer :privacy, null: false, default: 0
       t.belongs_to :user, type: :uuid
 
       t.timestamps
